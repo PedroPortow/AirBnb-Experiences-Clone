77248fe40e65cffe6b3bdda63a3096d7e7632da6
PedroPortow <pedrolportow@gmail.com>
2022-02-11 11:10:07 -0300

    primeiro commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+export default function Navbar(){
+    return (
+        <div className="navbar">
+            <img src="../images/airbnblogo.png" alt="" className="logo" />
+
+
+
+        </div>
+
+        
+    )
+}
